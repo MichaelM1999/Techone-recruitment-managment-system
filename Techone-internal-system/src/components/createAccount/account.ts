3e58334200b8d79f3aebc4834d13596485a977fd
michael mariea <mariea.michael@gmail.com>
2020-06-12 13:57:58 -0700

    authentification on create account

--- Techone-internal-system/src/components/createAccount/account.ts
+++ Techone-internal-system/src/components/createAccount/account.ts
@@ -9,11 +9,42 @@ import { loginRoutes } from '../../apis/login-api';
 export class Account {
     constructor(private API: loginRoutes){
     }
-    createUser(user){
-        console.log(user);
+
+
+    createUserCheck(user){
+        if (user.username === '' || user.password === '' || user.Email === '' || user.Firstname === '' || user.Lastname === ''){
+            window.alert('Must fill in all required fields.');
+        }
+        if (this.checkSymbol(user.username) === false){
+            window.alert('username must have at leasst one symbol.');
+        }
+        if (this.checkSymbol(user.password) === false){
+            window.alert('password must contain at least one symbol.');
+        }
+        if (this.checkSymbol(user.Firstname) === true || this.checkSymbol(user.Lastname) === true){
+            window.alert('Firstname & Lastname must be only letters.');
+        }
+        if (user.Email.includes("@") === false){
+            window.alert('Must enter a valid Email Address.')
+        }
+        else {
+            this.CreateUser(user);
+        }
+    }
+
+
+    checkSymbol(query) {
+        var symbol = new RegExp(/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/); //acceptable chars
+        if (symbol.test(query)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    CreateUser(user) {
         this.API.createUser(user).subscribe((res) => {
             if(res === null) {
-                window.alert("incorrect username and password");
+                window.alert("User already Exists");
                 console.log(res);
             } else {
                 sessionStorage.setItem('username', res['username']);
@@ -23,3 +54,4 @@ export class Account {
         })
     }
 }
+//
